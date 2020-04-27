b0ee4cce14ccde39a03fe23f251d6d05206a4275
David Martinez <david.mtz00@hotmail.com>
2020-04-27 17:42:09 -0500

    Added clamp method to _ object. Successfully passed all tests.

--- _.js
+++ _.js
@@ -1,6 +1,10 @@
-
-
-
+_ = {
+    clamp(number, lower, upper) {
+        const lowerClampedValue = Math.max(number, lower);
+        const clampedValue = Math.min(lowerClampedValue, upper);
+        return clampedValue;
+    }
+}
 
 
 // Do not write or modify code below this line.
