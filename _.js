4d4d0cfda64e5c96aab0986e3f260fa03c90ab7a
David Martinez <david.mtz00@hotmail.com>
2020-04-28 17:52:55 -0500

    Added inRange method to _ object. Successfully passed all tests.

--- _.js
+++ _.js
@@ -3,9 +3,19 @@ _ = {
         const lowerClampedValue = Math.max(number, lower);
         const clampedValue = Math.min(lowerClampedValue, upper);
         return clampedValue;
+    },
+    inRange(number, start, end) {
+        if (typeof end === "undefined") {
+            end = start;
+            start = 0;
+        } else if (start > end) {
+            const temp = start;
+            start = end;
+            end = temp;
+        }
+        return start <= number && number < end;
     }
 }
-
 
 // Do not write or modify code below this line.
 module.exports = _;
